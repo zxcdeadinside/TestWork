f28f11dbf5d12a211d0b2019819371e0b44b3d20
zxcdeadinside <bidros2002@mail.ru>
2023-03-28 15:41:34 +0700

    Add project files.

---
+++ TestWork/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Tc;
+            Console.Write("Введите температуру окружающей среды:\nTc = ");
+            double.TryParse(Console.ReadLine(), out Tc);
+        }
+    }
+}
